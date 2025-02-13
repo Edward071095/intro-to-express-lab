156a5b5feec6041402ee6f8fa7b06684a18054e6
EdwardW710 <EdwardW710@yahoo.com>
2025-02-13 17:04:47 -0500

    exercise 4 complete

--- server.js
+++ server.js
@@ -10,6 +10,16 @@ const collectibles = [
     { name: 'vintage 1970s yogurt SOLD AS-IS', price: 0.99 }
   ];
 
+  const shoes = [
+    { name: "Birkenstocks", price: 50, type: "sandal" },
+    { name: "Air Jordans", price: 500, type: "sneaker" },
+    { name: "Air Mahomeses", price: 501, type: "sneaker" },
+    { name: "Utility Boots", price: 20, type: "boot" },
+    { name: "Velcro Sandals", price: 15, type: "sandal" },
+    { name: "Jet Boots", price: 1000, type: "boot" },
+    { name: "Fifty-Inch Heels", price: 175, type: "heel" }
+];
+
 
 //exercise one
 
@@ -32,14 +42,33 @@ const collectibles = [
 // });
 
 //exercise 3
-app.get('/collectibles/:index', (req, res) => {
-    if (!collectibles[req.params.index]) {
-        res.send('This item is not yet in stock. Check back soon!')
+// app.get('/collectibles/:index', (req, res) => {
+//     if (!collectibles[req.params.index]) {
+//         res.send('This item is not yet in stock. Check back soon!')
+//     } else {
+//         res.send(`So, you want the shiny ball ${collectibles[req.params.index].name}? For ${collectibles[req.params.index].price}, it can be yours!`)
+//     }
+//     res.send(req.params.index)
+// });
+
+//exercise 4
+
+app.get('/shoes', (req, res) => {
+    if (req.query['min-price']) {
+       const expensiveShoes = shoes.filter(shoe => shoe.price > req.query['min-price']);
+        res.send(expensiveShoes); 
+    } else if (req.query['max-price']) {
+        const cheapShoes = shoes.filter(shoe => shoe.price < req.query['max-price']);
+        res.send(cheapShoes)
+    } else if (req.query.type) {
+        const shoesOfAType = shoes.filter(shoe => shoe.type === req.query.type);
+        res.send(shoesOfAType)
     } else {
-        res.send(`So, you want the shiny ball ${collectibles[req.params.index].name}? For ${collectibles[req.params.index].price}, it can be yours!`)
+        res.send(shoes);
     }
-    res.send(req.params.index)
 });
+
+
 
 // Listen for requests on port 3000
 app.listen(3000, () => {
