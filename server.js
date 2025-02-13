972cbe58a621d04733c417a50fc07438694d2ed8
EdwardW710 <EdwardW710@yahoo.com>
2025-02-13 16:11:59 -0500

    exceris 2

--- server.js
+++ server.js
@@ -6,20 +6,23 @@ const app = express()
 
 //exercise one
 
-app.get('/', (req, res) => {
-    res.send("Hello");
- });
+// app.get('/', (req, res) => {
+//     res.send("Hello");
+//  });
 
-app.get('/greetings/:username', (req, res) => {
-    res.send(`<h1>What a delight it is to see you once more ${req.params.username}</h1>`)
-});
-
-
-// app.get('/roll/:number', (req, res) => {
-// const randomNumber = Math.floor(Math.random() * (req.params.randomNumber + 1))
-// res.send(`You rolled a ${randomNumber}</h1>`)
+// app.get('/greetings/:username', (req, res) => {
+//     res.send(`<h1>What a delight it is to see you once more ${req.params.username}</h1>`)
 // });
 
+// exercise two
+app.get('/roll/:number', (req, res) => {
+    if (isNaN(req.params.number)) {
+    res.send('you must specify a number!'); 
+} else {
+    let randomNumber = Math.floor(Math.random() * req.params.number)
+    res.send(`You rolled a ${randomNumber}`)
+}
+});
 
 // Listen for requests on port 3000
 app.listen(3000, () => {
